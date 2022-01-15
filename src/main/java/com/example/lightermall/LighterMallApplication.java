a000e6288ed1851e3acc65a47deb2897c5ee8359
rongqin <shenrongqin@iftech.io>
2022-01-15 20:49:56 +0800

    feat: add basic dependencies

---
+++ src/main/java/com/example/lightermall/LighterMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.lightermall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LighterMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LighterMallApplication.class, args);
+    }
+
+}
